a9dbe58b70b67a20fe634e0fe3cc03427128ecbc
Alexander Johnston <Aliics@hotmail.com>
2020-11-28 23:44:52 +1300

    projection for slices/arrays

--- proj.go
+++ proj.go
@@ -10,7 +10,14 @@ func projOf(i interface{}) string {
 	var fields []string
 	for i := 0; i < value.NumField(); i++ {
 		fieldName := value.Type().Field(i).Name
-		if f := value.Field(i); f.Kind() == reflect.Struct {
+		if f := value.Field(i); f.Kind() == reflect.Slice || f.Kind() == reflect.Array {
+			valueType := reflect.New(f.Type().Elem()).Elem()
+			if valueType.Kind() == reflect.Struct {
+				fields = append(fields, fieldName + "." + projOf(valueType.Interface()))
+			} else {
+				fields = append(fields, fieldName)
+			}
+		} else if f.Kind() == reflect.Struct {
 			fields = append(fields, fieldName + "." + projOf(f.Interface()))
 		} else {
 			fields = append(fields, fieldName)
--- proj_test.go
+++ proj_test.go
@@ -19,3 +19,20 @@ func Test_projOf_simpleWithNested(t *testing.T) {
 		t.Fatalf("expected %v, got %v", ex, p)
 	}
 }
+
+func Test_projOf_arrays(t *testing.T) {
+	type Dog struct {
+		Name string
+	}
+	type Store struct {
+		Names   []string
+		Buddies []Dog
+	}
+
+	p := projOf(Store{})
+
+	ex := "Names,Buddies.Name"
+	if p != ex {
+		t.Fatalf("expected %v, got %v", ex, p)
+	}
+}
