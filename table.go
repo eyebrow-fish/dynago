c38c80ab4dd4cad9e0863020bc1014fc163167c8
Alexander Johnston <Aliics@hotmail.com>
2020-11-30 21:52:23 +1300

    table docs

--- table.go
+++ table.go
@@ -7,25 +7,63 @@ import (
 	"reflect"
 )
 
+// A Table is a DynamoDb table client.
+//
+// A Table is the primary way to interact with DynamoDb
+// and all queries and mutations are served through it.
+//
+// * Note that all tables initialized with the same
+// aws.Config will reuse the same dynamodb.DynamoDB client.
 type Table struct {
 	name     string
 	dataType interface{}
+	conf     *aws.Config
 }
 
+// NewTable initializes a Table with the default
+// aws.Config.
+//
+// The name parameter is the name of the table in AWS.
+//
+// The dataType parameter should contain an empty
+// interface used for the queries and mutations of
+// the table.
+//
+// See NewTableWithConfig for more configuration.
 func NewTable(name string, dataType interface{}) (*Table, error) {
 	return NewTableWithConfig(name, dataType, *aws.NewConfig())
 }
 
+// NewTableWithConfig allows custom a aws.Config to be
+// passed in. Variables such as Region and Endpoint can be
+// configured when using this constructor. Otherwise, the
+// behavior is the same as NewTable.
+//
+// * Note that all tables initialized with the same
+// aws.Config will reuse the same dynamodb.DynamoDB client.
 func NewTableWithConfig(name string, dataType interface{}, conf aws.Config) (*Table, error) {
-	var err error
-	sess, err = session.NewSession(&conf)
-	if err != nil {
-		return nil, err
+	var confKey *aws.Config
+	for c, _ := range dynamoConf {
+		if reflect.DeepEqual(*c, conf) {
+			confKey = c
+		}
 	}
-	dynamo = dynamodb.New(sess)
-	return &Table{name, dataType}, nil
+	if confKey == nil {
+		sess, err := session.NewSession(&conf)
+		if err != nil {
+			return nil, err
+		}
+		dynamoConf[&conf] = dynamodb.New(sess)
+		confKey = &conf
+	}
+	return &Table{name, dataType, confKey}, nil
 }
 
+// Query is a method which queries items on a Table.
+//
+// The response of Query will be items which matches the
+// Cond(s) given. Additionally, the structure will match
+// the dataType parameter given to the Table constructor.
 func (t *Table) Query(cons ...Cond) ([]interface{}, error) {
 	keyCons := make(map[string]*dynamodb.Condition)
 	for _, v := range cons {
@@ -45,13 +83,18 @@ func (t *Table) Query(cons ...Cond) ([]interface{}, error) {
 		KeyConditions:        keyCons,
 		ProjectionExpression: &proj,
 	}
-	resp, err := dynamo.Query(&q)
+	resp, err := t.dynamoClient().Query(&q)
 	if err != nil {
 		return nil, err
 	}
 	return t.buildResp(resp.Items)
 }
 
+// Delete is a method for deleting items of a Table.
+//
+// When using Delete, the keys parameters determine which
+// items are deleted. The more keys provided, the more
+// fine grained the deletions are.
 func (t *Table) Delete(keys map[string]Val) error {
 	avKeys, err := toAvMap(keys)
 	if err != nil {
@@ -61,10 +104,14 @@ func (t *Table) Delete(keys map[string]Val) error {
 		TableName: &t.name,
 		Key:       avKeys,
 	}
-	_, err = dynamo.DeleteItem(&d)
+	_, err = t.dynamoClient().DeleteItem(&d)
 	return err
 }
 
+// Put is a method for inserting items into a Table.
+//
+// All items given to Put must match the structure of
+// the dataType of Table.
 func (t *Table) Put(item map[string]Val) error {
 	avItem, err := toAvMap(item)
 	if err != nil {
@@ -74,8 +121,12 @@ func (t *Table) Put(item map[string]Val) error {
 		TableName: &t.name,
 		Item:      avItem,
 	}
-	_, err = dynamo.PutItem(&p)
+	_, err = t.dynamoClient().PutItem(&p)
 	return err
+}
+
+func (t *Table) dynamoClient() *dynamodb.DynamoDB {
+	return dynamoConf[t.conf]
 }
 
 func (t *Table) buildResp(items []map[string]*dynamodb.AttributeValue) ([]interface{}, error) {
@@ -95,6 +146,5 @@ func (t *Table) buildResp(items []map[string]*dynamodb.AttributeValue) ([]interf
 }
 
 var (
-	sess   *session.Session
-	dynamo *dynamodb.DynamoDB
+	dynamoConf = make(map[*aws.Config]*dynamodb.DynamoDB)
 )
